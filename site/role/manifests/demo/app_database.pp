9d0c745e27d3f734efc7fd214e901a8083d40468
Walter Gildersleeve <walter@gildersleeve.eu>
2018-06-19 21:52:16 +0200

    added database role

---
+++ site/role/manifests/demo/app_database.pp
@@ -0,0 +1,11 @@
+# role::demo::postgresql
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include role::demo::postgresql
+class role::demo::postgresql {
+  include profile::demo::postgresql
+}
