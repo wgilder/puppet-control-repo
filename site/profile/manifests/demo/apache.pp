9cc68f54af0a389a4ed20a553b42ee65bed898e7
Walter Gildersleeve <walter@gildersleeve.eu>
2018-06-19 20:16:50 +0200

    complexified apache install

--- site/profile/manifests/demo/apache.pp
+++ site/profile/manifests/demo/apache.pp
@@ -7,5 +7,14 @@
 # @example
 #   include profile::demo::apache
 class profile::demo::apache {
-  class { 'apache': }
+  class { 'apache': 
+    default_vhost=> true,
+  }
+
+  file { '/var/www/html/index.html':
+    ensure => file,
+    source => 'puppet:///modules/profile/default.html',
+    mode   => '0777',
+    owner  => 'root',
+  }
 }
